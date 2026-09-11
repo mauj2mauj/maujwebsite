e1155f52f96851a19b1491c6cb94e7feea0aa98f
mauj2mauj <mauj2mauj@gmail.com>
2026-09-11 17:05:31 +0530

    Hide Login and Register

--- src/pages/mauj/MaujSection.jsx
+++ src/pages/mauj/MaujSection.jsx
@@ -113,7 +113,7 @@ export default function MaujSection() {
         <div className="au-loading"><Loader2 className="au-spin" size={22} /> Loading…</div>
       )}
 
-      
+      {/* {!loading && !user && <AuthForms />} */}
 
       {!loading && user && (
         <>
@@ -144,7 +144,7 @@ export default function MaujSection() {
 
       <Rule />
 
-      <AdminGate />
+      {/* <AdminGate /> */}
     </div>
   );
 }
